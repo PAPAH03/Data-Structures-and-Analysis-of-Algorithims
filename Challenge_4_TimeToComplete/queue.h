4afa42bae4a7ad5dc85a2f9c64aba25c5835bc21
PAPAH03 <160181628+PAPAH03@users.noreply.github.com>
2024-04-16 21:21:59 -0500

    Update queue.h

--- Challenge_4_TimeToComplete/queue.h
+++ Challenge_4_TimeToComplete/queue.h
@@ -4,7 +4,38 @@
 #ifndef STACK_H
 #define STACK_H
 
-#pragma once
+#pragma once#pragma once
+#include "node.h"
+#include <ostream>
+
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+
+class LinkedList
+{
+private:
+    Node* head, * tail;
+public:
+    LinkedList() : head(nullptr), tail(nullptr) {}
+    ~LinkedList();
+
+    void append(int data);
+    void prepend(int data);
+    void appendRandomNumbers(int count, int maxRand);
+
+    bool remove(int val);
+    bool tailRemove(int val);
+    bool search(int data);
+
+    void display(std::ostream& os);
+    void tailDisplay(std::ostream& os);
+
+    friend std::ostream& operator<<(std::ostream& os, LinkedList& right);
+};
+
+#endif // LINKEDLIST_H
+
 struct Node{
     public:
         int data;
